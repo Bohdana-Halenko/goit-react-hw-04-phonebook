874a7463e2abd3c37fd6664d4c73e83a081109d1
bohdana <halenkobohdana@gmail.com>
2022-04-12 00:24:25 +0300

    hw4

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,70 +1,69 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import propTypes from 'prop-types';
-import { nanoid } from 'nanoid';
 import s from './ContactForm.module.css';
 
-class ContactForm extends Component {
-    state = {
-        name: '',
-        number: ''
+function ContactForm({onSubmit}) {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+
+    const changeInputName = event => {
+        setName(event.currentTarget.value);
+    };
+  
+    const changeInputNumber = event => {
+        setNumber(event.currentTarget.value);
     };
 
-    hendleChange = event => {
-        const { name, value } = event.currentTarget;
-        this.setState({[name] : value, id: nanoid(),});
-    }
-
-    hendleSubmit = event => {
+    const hendleSubmit = event => {
         event.preventDefault();
-        this.props.onSubmit(this.state);
-        this.reset();
+        const data = { name, number };
+        onSubmit(data);
+        reset();
     };
 
-    reset = () => {
-        this.setState({ name:'', number:'' });
+    const reset = () => {
+        setName('');
+        setNumber('');
     };
 
-
-    render() {
-        return (
-            <form onSubmit={this.hendleSubmit} className={s.formWrap}>
-                <div className={s.inputWrap}>
-                    <label className={s.label}>Name</label>
-                    <input
-                        className={s.input}
-                        value={this.state.name}
-                        onChange={this.hendleChange}
-                        type="text"
-                        name="name"
-                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                        required
-                    />  
-                </div>
-                <div className={s.inputWrap}>
-                    <label className={s.label}>Number</label>
-                    <input
-                        className={s.input}
-                        value={this.state.number}
-                        onChange={this.hendleChange}
-                        type="tel"
-                        name="number"
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                        required
-                    />
-                </div>   
-                <button className={s.button} type='submit'>
-                    Add contact
-                </button>
-            </form>
-        );
-    }
+    return (
+        <form onSubmit={hendleSubmit} className={s.formWrap}>
+            <div className={s.inputWrap}>
+                <label className={s.label}>Name</label>
+                <input
+                    className={s.input}
+                    value={name}
+                    onChange={changeInputName}
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                />  
+            </div>
+            <div className={s.inputWrap}>
+                <label className={s.label}>Number</label>
+                <input
+                    className={s.input}
+                    value={number}
+                    onChange={changeInputNumber}
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                />
+            </div>   
+            <button className={s.button} type='submit'>
+                Add contact
+            </button>
+        </form>
+    );
 }
 
 ContactForm.propTypes = {
-  name: propTypes.string,
-  number: propTypes.number,
+  name: propTypes.string.isRequired,
+  number: propTypes.number.isRequired,
 };
 
 export default ContactForm;
